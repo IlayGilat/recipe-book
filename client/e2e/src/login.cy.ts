d14c1259efa910d1e513834bca2fa15862213ca2
ilaygilat <gilat.ilay@gmail.com>
2025-05-04 11:54:23 +0300

    dads

--- client/e2e/src/login.cy.ts
+++ client/e2e/src/login.cy.ts
@@ -2,13 +2,11 @@
 
 describe('User Login', () => {
   beforeEach(() => {
-    // Visit the login page
     cy.visit('localhost:4200/auth');
     cy.get('button[id="switch"]').click();
   });
 
   it('should display login form', () => {
-    // Check if login form elements are visible
     cy.get('form').should('exist');
     cy.get('input[type="email"]').should('be.visible');
     cy.get('input[type="password"]').should('be.visible');
